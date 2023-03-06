acd4302b1c5516eb66e9e5070beda2afdc5b2872
Phuong vy <vy002359@gmail.com>
2023-03-06 21:15:16 +0700

    Search,findlength,reverse in Linkedlist

--- Linked_list/linkedlist.py
+++ Linked_list/linkedlist.py
@@ -40,52 +40,77 @@ class SLinkedList :
           position.nextval = Newnode #make next of position node as new node
      def deletion(self,position):
           temp = self.head #store header
-          if self.head is not None: #Check if list is empty
+          if self.head is not None: #Check if list is not empty
+               #If head node itself holds the key to be deleted
                if temp.data == position:
                     self.head = temp.nextval
                     temp = None
                     return
+          #Search for the node to be deleted,keep track of the previous node as we need to change
           while temp is not None :
                if temp.data == position:
                     break          
                prev = temp 
                temp = temp.nextval
+          #If key was not present in linked list
           if temp is None :
                return 
+          #Unlink the previous node from the linked list
           prev.nextval = temp.nextval
           temp = None
-     '''This is a Python code for a function called "swapPairs" which takes a singly-linked list as input and swaps every two adjacent nodes and returns the modified linked-list.
+     def swapNodes(self,x,y):
+          if x == y :
+               return
+          prevX = None
+          currentX = self.head
+          while currentX is not None and currentX.data!= x:
+               prevX = currentX
+               currentX = currentX.nextval
+          prevY = None
+          currentY = self.head
+          while currentY is not None and currentY.data!= y:
+               prevY = currentY
+               currentY = currentY.nextval
+          if currentX is None or currentY is None:
+               return 
+          if prevX is not None:
+               prevX.nextval = currentY
+          else :
+               self.head = currentY
+          if prevY is not None:
+               prevY.nextval = currentX
+          else :
+               self.head = currentX
+          
+          temp = currentX.nextval
+          currentX.nextval = currentY.nextval
+          currentY.nextval = temp 
+     def find_lengthOfLinkedList(self):
+          temp = self.head #Initialize temp 
+          count = 1 #Initialize count
 
-In the beginning, it checks for the base cases in which the head is None or there is only one node in the linked list.
-
-The swapping takes place by using three variables tmp1, tmp2, and tmpx. initially, tmp1 and tmp2 point to the first two nodes of the linked list. Then, it swaps their values and updates the tmp1 and tmp2 to the next two nodes. It keeps doing this until reaching the end of the linked list. Finally, it returns the head of the modified linked list.
-
-Note: This function uses an external Python class ListNode to define the structure of a node in the linked list'''
-     def swapPair(self,head : Optional[Node]) -> Optional[Node]:
-          if head is None :
-               return None
-          if head.nextval is None :
-               return head
-          temp1 = head
-          temp2 = head.nextval
-          while temp1 is not None :
-               if temp1 is not None and temp2 is not None :
-                    print(temp1.data)
-                    print(temp2.data)
-
-                    tempval = temp1.data
-                    tempval = temp1 
-                    temp1.val = temp2.data
-                    temp2.data = tempval
-
-                    temp1 = temp2.nextval
-                    if temp2.nextval is not None :
-                         temp2 = temp2.nextval.nextval
-                    else :
-                         temp2 = None
-               else :
-                    break 
-          return head
+          #Loop while end of linked list is not reached
+          while temp is not None:
+               count += 1
+               temp = temp.nextval
+          return count
+     def Search_element(self,data):
+          current = self.head #Initialize current to head 
+          #lopp till current not equal to None
+          while current is not None:
+               if current.data == data:
+                    return True #data is founded 
+               current = current.nextval
+          return False #data is not founded
+     def reverse_LinkedList(self):
+          prev = None 
+          current = self.head
+          while current is not None:
+               nextval = current.nextval
+               current.nextval = prev
+               prev = current
+               current = nextval
+          self.head = prev
 if __name__ == "__main__":
      list1 = SLinkedList()
      list1.head = Node("Mon")
@@ -111,11 +136,17 @@ if __name__ == "__main__":
      list1.deletion("Tue")
      print("List after deletion:")
      list1.listprint() 
-     list1.head = list1.swapPair(list1.head.nextval.nextval)
-     list1.head.nextval = list1.head
+     print("List after swapping nodes:")
+     list1.swapNodes("Mon","Fri")
      list1.listprint() 
-
-
+     print("Length of the linked list is : ", list1.find_lengthOfLinkedList())
+     if list1.Search_element("Sat") == True:
+          print("Element is present in the linked list")
+     else :
+          print("Element is not present in the linked list")
+     list1.reverse_LinkedList()
+     print("List after reversing the linked list:")
+     list1.listprint() 
 
 
 
